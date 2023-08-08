0540190409307d2f0f07ef69050ff60de5ace566
Khyrstina Smith <Khyrstinasmith@gmail.com>
2023-08-08 10:14:23 -0400

    Reworked navigation as sidepanel

--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,7 @@
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
 
-var skipLink = document.querySelector('.skip-link');
-skipLink.addEventListener('click', function (e) {
-  e.preventDefault();
-  document.querySelector(skipLink.getAttribute('href')).focus();
-});
-
-var menu = document.querySelector('#main-nav');
-document.querySelector('#menu-trigger').addEventListener('click', function (e) {
-  menu.classList.toggle('active');
-});
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}

0e5ae866f9fedd057c0fca55848593a5a7f17dc4
Khyrstina Smith <khyrstinasmith@gmail.com>
2023-08-09 12:53:24 -0400

    Added preliminary api setup

--- js/stateSearch.js
+++ js/stateSearch.js
@@ -1,7 +1,53 @@
 const stateSelect = document.getElementById('stateId');
 const textBox = document.getElementById('textBox');
 
-stateSelect.addEventListener('change', (event) => {
-const selectedState = stateSelect.value;
-textBox.value = selectedState;
+let apiKey = "";
+
+stateSelect.addEventListener('change', async (event) => {
+    const selectedState = stateSelect.value;
+    textBox.value = selectedState;
+    await renderParks(selectedState);
 });
+
+async function getParks(selectedState) {
+    let apiUrl = `https://developer.nps.gov/api/v1/parks?limit=10&q=${selectedState}&api_key=${apiKey}`;
+    try {
+        let resp = await fetch(apiUrl);
+        let data = await resp.json();
+        console.log(apiUrl);
+        return data.data;
+    } 
+    catch (error) {
+        console.log(error);
+        console.log(apiUrl);
+    }
+}
+
+async function renderParks(selectedState) {
+    let parks = await getParks(selectedState);
+    let html = '';
+
+    parks.forEach(park => {
+        let htmlSegment = `
+            <div class="park">
+                <img src="${park.images[0]?.url || ''}">
+                <h2>${park.fullName}</h2>
+                <div class="info">
+                    <p>
+                        ${park.url}
+                        ${park.addresses[0]?.city || ''}
+                        ${park.addresses[0]?.postalCode || ''}
+                        ${park.contacts.phoneNumbers[0]?.phoneNumber || ''}
+                        ${park.description || ''}
+                    </p>
+                </div>
+            </div>
+        `;
+        html += htmlSegment;
+    });
+
+    let jsonContainer = document.querySelector('.jsonContainer');
+    jsonContainer.innerHTML = html;
+}
+
+renderParks(stateSelect.value);

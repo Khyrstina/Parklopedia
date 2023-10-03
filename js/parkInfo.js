37c97f4ddddebc2d899e0e79905103d65c456ec4
Khyrstina Smith <khyrstinasmith@gmail.com>
2023-10-03 12:19:52 -0400

    added imports and base code for weather

--- js/parkInfo.js
+++ js/parkInfo.js
@@ -1,6 +1,9 @@
 import { getParkInfo, getLatLon } from './parkAPI.js';
 import { initializeSlides, plusSlides } from './slideshow.js';
 import { getAlertsInformation } from './alerts.js';
+import { getWeatherInfo } from './weatherAPI.js';
+import { findCorrectIcon } from './weatherIcon.js';
+
 
 
 let latitude = '';
@@ -87,11 +90,84 @@ async function fetchParkDetails(parkId) {
 
 
   // Begin .weather 
-  const weatherStats = document.querySelector('.weather');
   latitude = park[0].latitude;
   longitude = park[0].longitude;
-  weatherStats.textContent = getLatLon(latitude, longitude);
 
+  const weatherData = await getWeatherInfo(latitude, longitude);
+  const weatherStats = document.querySelector('.weather');
+  
+  const conditionCode = weatherData.current.condition.code;
+
+
+
+
+  const dateStrings = [weatherData.forecast.forecastday[1].date, weatherData.forecast.forecastday[2].date, weatherData.forecast.forecastday[3].date];
+
+  function formatDate(dateStrings) {
+    let parts = dateStrings.split('-');
+    let month = parts[1];
+    let day = parts[2];
+    return month + '/' + day;
+  }
+
+  let formattedDateStrings = [];
+
+  dateStrings.forEach(function(dateStrings) {
+    var formattedDate = formatDate(dateStrings);
+    formattedDateStrings.push(formattedDate);
+
+  });
+
+ 
+
+
+
+
+
+  let weatherHTML = `
+  <div class="weatherContainer">
+  <div > <img class="mainWeatherImg" src="" alt="">  </div>
+  <div class="currentTemp">${weatherData.current.temp_f}&deg;F</div>
+  <div class="highAndLowTemp">
+  <div class="maxTemp">   
+  ${weatherData.forecast.forecastday[0].day.maxtemp_f}&deg;F </div>
+  <div class="minTemp">
+  ${weatherData.forecast.forecastday[0].day.mintemp_f}&deg;F </div>
+  </div>
+</div>
+  <div class="threeDayForecastBox">
+  <div class="datesBox">    </div>
+    <div class="dateOne">${formattedDateStrings[0]}</div>
+    <div class="dateTwo">${formattedDateStrings[1]}</div>
+    <div class="dateThree">${formattedDateStrings[2]}</div>
+
+    <div class="weatherImgBox">    </div>
+    <div class="weatherImgOne"></div>
+    <div class="weatherImgTwo"></div>
+    <div class="weatherImgThree"></div>
+
+    <div class="highLowBox">    </div>
+    <div class="highLowOne">${weatherData.forecast.forecastday[1].day.maxtemp_f}&deg;F <br> ${weatherData.forecast.forecastday[1].day.mintemp_f}&deg;F</div>
+    <div class="highLowTwo">${weatherData.forecast.forecastday[2].day.maxtemp_f}&deg;F <br> ${weatherData.forecast.forecastday[2].day.mintemp_f}&deg;F</div>
+    <div class="highLowThree">${weatherData.forecast.forecastday[3].day.maxtemp_f}&deg;F <br> ${weatherData.forecast.forecastday[3].day.mintemp_f}&deg;F</div>
+
+
+</div>
+  `;
+
+
+  weatherStats.innerHTML = weatherHTML;
+
+  
+  if (conditionCode) {
+    let mainWeatherImg = findCorrectIcon(conditionCode);
+    document.getElementsByClassName("currentTemp").style.backgroundImage=`url(${mainWeatherImg})`;
+    console.log("Condition Code:", conditionCode);
+  } else {
+    console.error("Element with class 'mainWeatherImg' not found in the DOM.");
+    console.log("Condition Code:", conditionCode);
+    console.log("Weathericon src set", mainWeatherImg);
+  }
 
   // Begin .contact
   const contactInformation = document.querySelector('.contact');

4bf77d0b4111fd2fdd226669cc9e5e5d4b80912d
Khyrstina Smith <khyrstinasmith@gmail.com>
2023-10-13 11:17:45 -0400

    new navigation menu

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,20 @@
-function openNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
+const sideMenu = document.getElementById('mySidepanel');
+const hamburgerMenuButton = document.getElementById('hamburgerMenu');
+
+hamburgerMenuButton.addEventListener('click', () => {
+  if (parseInt(sideMenu.style.width) < 5) {
+    sideMenu.style.display = "flex";
+    sideMenu.style.width = "fit-content";
+  } else {
+    sideMenu.style.width = "0";
+    sideMenu.style.display = "none";
+  }
+});
+
+/* function openNav() {
+  document.getElementById("mySidepanel").style.width = "15rem";
 }
 
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
-}
+} */

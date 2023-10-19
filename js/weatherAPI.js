b91be98fa30109ec22ab1d421f9c1c178502f9d5
Khyrstina Smith <khyrstinasmith@gmail.com>
2023-10-19 13:36:30 -0400

    modified url string

--- js/weatherAPI.js
+++ js/weatherAPI.js
@@ -2,7 +2,7 @@ import { weatherApiKey } from './config.js';
 
 
 export async function getWeatherInfo(latitude, longitude) {
-    const weatherApiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey} &q=${latitude + ',' + longitude}&days=4&aqi=no&alerts=yes`;
+    const weatherApiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${latitude + ',' + longitude}&days=4&aqi=no&alerts=yes`;
     console.log(weatherApiUrl);
     console.log(latitude + ',' + longitude);
     try {

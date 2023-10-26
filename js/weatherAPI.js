c6ee7355f7d81554f0a2d341c14f0ddc30331344
Khyrstina Smith <khyrstinasmith@gmail.com>
2023-10-26 12:11:29 -0400

    minor spacing changes

--- js/weatherAPI.js
+++ js/weatherAPI.js
@@ -1,20 +1,17 @@
-import { weatherApiKey } from './config.js';
-
+import { weatherApiKey } from "./config.js";
 
 export async function getWeatherInfo(latitude, longitude) {
-    const weatherApiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${latitude + ',' + longitude}&days=4&aqi=no&alerts=yes`;
-    console.log(weatherApiUrl);
-    console.log(latitude + ',' + longitude);
-    try {
-        let resp = await fetch(weatherApiUrl);
-        let data = await resp.json();
+  const weatherApiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${
+    latitude + "," + longitude
+  }&days=4&aqi=no&alerts=yes`;
+  console.log(weatherApiUrl);
+  console.log(latitude + "," + longitude);
+  try {
+    let resp = await fetch(weatherApiUrl);
+    let data = await resp.json();
 
-        return data; 
-
-
-    }
-    catch (error) {
-        console.log(error);
-    }
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
-

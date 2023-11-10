318a8f099be09bb08a5df6275f7819df8f2fa09e
Khyrstina Smith <khyrstinasmith@gmail.com>
2023-11-10 10:55:21 -0500

    Changed width of nav

--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,13 @@
 const sideMenu = document.getElementById('mySidepanel');
 const hamburgerMenuButton = document.getElementById('hamburgerMenu');
 
+
 hamburgerMenuButton.addEventListener('click', () => {
   if (parseInt(sideMenu.style.width) < 5) {
     sideMenu.style.display = "flex";
-    sideMenu.style.width = "fit-content";
+    sideMenu.style.width = "98%";
   } else {
     sideMenu.style.width = "0";
     sideMenu.style.display = "none";
   }
 });
-

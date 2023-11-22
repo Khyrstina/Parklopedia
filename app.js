3d05dd7a1a5706309d0ea5499b619996c013684e
Khyrstina Smith <khyrstinasmith@gmail.com>
2023-11-22 17:10:22 -0500

    Add aboutRouter to app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var searchRouter = require('./routes/search');
 var parkRouter = require('./routes/parkRoute');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/search', searchRouter); 
 app.use('/parkRoute', parkRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

d871391752e5c706a6530515385640be94aac131
Khyrstina Smith <khyrstinasmith@gmail.com>
2023-11-23 12:52:36 -0500

    Add expandAbout.js file with code to expand and collapse divs

---
+++ public/javascripts/expandAbout.js
@@ -0,0 +1,51 @@
+const reqOne = document.getElementById('reqOne');
+const reqTwo = document.getElementById('reqTwo');
+const reqThree = document.getElementById('reqThree');
+const reqFour = document.getElementById('reqFour');
+const reqFive = document.getElementById('reqFive');
+const reqSix = document.getElementById('reqSix');
+
+const reqDivOne = document.getElementById('reqDivOne');
+const reqDivTwoA = document.getElementById('reqDivTwoA');
+const reqDivTwoB = document.getElementById('reqDivTwoB');
+const reqDivThree = document.getElementById('reqDivThree');
+const reqDivFour = document.getElementById('reqDivFour');
+const reqDivFive = document.getElementById('reqDivFive');
+const reqDivSix = document.getElementById('reqDivSix');
+
+let divToExpand = '';
+
+document.addEventListener('click', function(event) {
+    switch (event.target.id) {
+        case 'reqOne':
+            showDivs(reqDivOne);
+            break;
+        case 'reqTwo':
+            showDivs(reqDivTwoA);
+            showDivs(reqDivTwoB);
+            break;
+        case 'reqThree':
+            showDivs(reqDivThree);
+            break;
+        case 'reqFour':
+            showDivs(reqDivFour);
+            break;
+        case 'reqFive':
+            showDivs(reqDivFive);
+            break;
+        case 'reqSix':
+            showDivs(reqDivSix);
+            break;
+        default:
+            break;
+    }
+});
+
+ function showDivs(divToExpand) {
+    if (divToExpand && divToExpand.style.display === "none") {
+        divToExpand.style.display = "block";
+    } else if (divToExpand) {
+        divToExpand.style.display = "none";
+    }
+}
+
